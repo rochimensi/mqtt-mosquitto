a370f381275f6376e628c01ef5e9dd93a9e88448
rochimensi <rochibass@gmail.com>
2018-06-04 23:32:13 -0300

    reformatted index.js

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ let processConnection = () => {
   prompt.get(['nameTopic'], (err, result) => {
     if(!result.nameTopic) return processConnection();
 
-  let nombre = result.nameTopic.split(';')[0];
-  let sala = result.nameTopic.split(';')[1];
-  client.subscribe(sala);
-  clients[client.options.clientId] = {
-    who: nombre,
-    topic: sala
-  };
-  prompt.get(['message'], processMessage);
-});
+    // clientName;topic
+    let clientName = result.nameTopic.split(';')[0];
+    let topic = result.nameTopic.split(';')[1];
+    client.subscribe(topic);
+    clients[client.options.clientId] = {
+      who: clientName,
+      topic: topic
+    };
+    prompt.get(['message'], processMessage);
+  });
 };
 
 let processMessage = (err, result) => {
   let payload = {
     name: clients[client.options.clientId].who,
     msg: result.message,
-    city: 'Mar del Plata',
     sent_at: new Date()
   };
   client.publish(clients[client.options.clientId].topic,  JSON.stringify(payload));
@@ -31,15 +31,15 @@ let processMessage = (err, result) => {
 
 client.on('connect', () => {
   prompt.start();
-processConnection();
+  processConnection();
 });
 
 client.on('message', (topic, message) => {
   let mensaje = message.toString();
-let payload = JSON.parse(mensaje);
-console.log(`[${payload.sent_at},${payload.city}]${payload.name}:${payload.msg}`);
-if(JSON.parse(mensaje).msg.indexOf("EXIT_ROOM") > -1) {
-  prompt.stop();
-  client.end();
-}
+  let payload = JSON.parse(mensaje);
+  console.log(`[${payload.sent_at},${payload.city}]${payload.name}:${payload.msg}`);
+  if(JSON.parse(mensaje).msg.indexOf("EXIT_ROOM") > -1) {
+    prompt.stop();
+    client.end();
+  }
 });
